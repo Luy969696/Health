f289866124056dbf78dee81ed32c327203182199
Luy969696_macbook <dnrdydsla@gmail.com>
2023-03-10 21:21:03 +0900

    0310

--- public/action.js
+++ public/action.js
@@ -1,16 +1,14 @@
 
 
-
-const rows = document.querySelectorAll("tbody tr");
-console.log(rows);
-
-
-
-rows.forEach(row=>{
-  row.addEventListener("click",()=>{
-    console.log(row.cells[0].textContent);
-  });
-
-});
-
-
+function Weight_Check(){
+  const Weight = document.querySelectorAll(".WeightItem input[name='group1']:checked");
+  const WeightCotent = document.getElementById("value");
+  let value = 0;
+  if(Weight.length >0){
+    value = Weight[0].value;
+  }
+  
+  
+  
+  WeightCotent.innerText = value;
+}
